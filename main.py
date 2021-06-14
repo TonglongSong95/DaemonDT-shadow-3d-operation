1fe533a64a67d57a80a7ecf5dac1a2f3c04b54df
TonglongSong95 <songt1@student.unimelb.edu.au>
2021-06-13 20:28:15 -0700

    fixed input json structure and exception output

--- main.py
+++ main.py
@@ -9,39 +9,46 @@ app = Flask(__name__)
 @app.route('/shadow-3d-operation/version')
 def appversion():
     return {
-        'version': '1.0.0.0',
-        'last update': '11/6/2021'
+        'version': '1.0.0.1',
+        'last update': '14/6/2021'
     }
 
 
 @app.route('/shadow-3d-operation/bec/devplan/<uuid>/envelope/<env_uuid>/shadowunion', methods=['POST'])
 def return_true_shadow(uuid, env_uuid):
-    if request.method == 'POST':
-        try:
-            data = request.get_json(force=True)
-            true_shadow = shadow_calc.true_shadow_geom(data['data'])
-            output = {
-                'code': 200,
-                'data': {
-                    "envTrueSV": m.write_mesh(true_shadow),
-                    "envTrueSVVolume": m.transform_mesh_to_pcs(true_shadow).volume
-                },
-                'status': 'success'
-            }
-            output = jsonify(output)
-            output.headers['token'] = data['data']['token']
-            return output
-        except Exception:
-            return {
-                'code': 400,
-                'status': f"error: invalid data"
-            }
+    try:
+        if request.method == 'POST':
+            try:
+                data = request.get_json(force=True)
+                true_shadow = shadow_calc.true_shadow_geom(data)
+                output = {
+                    'code': 200,
+                    'data': {
+                        "envTrueSV": m.write_mesh(true_shadow),
+                        "envTrueSVVolume": m.transform_mesh_to_pcs(true_shadow).volume
+                    },
+                    'status': 'success'
+                }
+                output = jsonify(output)
+                output.headers['token'] = data['data']['token']
+                return output
+            except Exception as e:
+                return {
+                    'code': 500,
+                    'status': f"error: {e}"
+                }
 
-    else:
+        else:
+            return {
+                'code': 500,
+                'status': 'Non POST request received'
+            }
+    except Exception as e:
         return {
             'code': 400,
-            'status': 'Non post request received'
+            'status': f"error: {e}"
         }
+
 
 
 if __name__ == '__main__':
